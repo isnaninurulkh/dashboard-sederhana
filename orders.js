987e02a5f6b4e37bcfe5aeea9c06d534ce0f1a90
isnaninurulkh <isnaninurulkh@gmail.com>
2023-10-07 22:33:56 +0700

    menambahkan beberapa file utama

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: 'Mini Drone',
+    productNumber: '98765',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Lavender Drone',
+    productNumber: '98765',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Pro Drone',
+    productNumber: '98765',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Camera Drone',
+    productNumber: '98765',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'GPS 4k Drone',
+    productNumber: '98765',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  }
+]
